16faf8c1b7007f5d728483e49e34b3d23fc14f2b
Markus Hauge <markus.hauge@hotmail.com>
2015-12-23 17:36:59 +0100

    Initialize MaterialSkin color scheme

--- src/LibLog.Client/LibLog.Client/MainForm.cs
+++ src/LibLog.Client/LibLog.Client/MainForm.cs
@@ -1,13 +1,29 @@
 ﻿using System;
+using MaterialSkin;
 using MaterialSkin.Controls;
 
 namespace LibLog.Client
 {
     public partial class MainForm : MaterialForm
     {
+        private MaterialSkinManager materialSkinManager;
+
         public MainForm()
         {
             this.InitializeComponent();
+            this.InitializeMaterialSkin();
+        }
+
+        private void InitializeMaterialSkin()
+        {
+            this.materialSkinManager = MaterialSkinManager.Instance;
+            this.materialSkinManager.ColorScheme = new ColorScheme(Primary.BlueGrey800,
+                                                                   Primary.BlueGrey900,
+                                                                   Primary.BlueGrey500,
+                                                                   Accent.LightBlue200,
+                                                                   TextShade.WHITE);
+
+
         }
     }
 }
